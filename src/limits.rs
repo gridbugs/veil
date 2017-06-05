8ccec42518b208f3dbd0c3af69eb7d4c7e5297f9
Stephen Sherratt <stephen@sherra.tt>
2017-06-06 01:30:40 +1000

    Add checked method to LimitsRect trait

--- src/limits.rs
+++ src/limits.rs
@@ -7,6 +7,16 @@ pub trait LimitsRect {
     fn y_min(&self) -> i32;
     fn y_max(&self) -> i32;
 
+    fn checked(&self, v: Vector2<i32>) -> Option<Vector2<i32>> {
+        if v.x >= self.x_min() && v.x <= self.x_max() &&
+            v.y >= self.y_min() && v.y <= self.y_max()
+        {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
     fn saturate(&self, mut v: Vector2<i32>) -> Vector2<i32> {
         v.x = cmp::max(self.x_min(), cmp::min(self.x_max(), v.x));
         v.y = cmp::max(self.y_min(), cmp::min(self.y_max(), v.y));
