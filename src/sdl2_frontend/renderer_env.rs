54756e839f6443d4f2416b0a55e52af94ba19054
Stephen Sherratt <stephen@sherra.tt>
2017-06-12 21:45:43 +1000

    Remove unnecessary sdl config

--- src/sdl2_frontend/renderer_env.rs
+++ src/sdl2_frontend/renderer_env.rs
@@ -1,7 +1,6 @@
 use sdl2::VideoSubsystem;
 use sdl2::video::WindowContext;
 use sdl2::render::{WindowCanvas, TextureCreator};
-use sdl2::video::GLProfile;
 
 pub struct RendererEnv {
     pub canvas: WindowCanvas,
@@ -11,15 +10,7 @@ pub struct RendererEnv {
 impl RendererEnv {
     pub fn new(width_px: u32, height_px: u32, video: &VideoSubsystem) -> Self {
 
-        let gl_attr = video.gl_attr();
-
-        gl_attr.set_context_profile(GLProfile::Core);
-        gl_attr.set_double_buffer(true);
-        gl_attr.set_multisample_buffers(1);
-        gl_attr.set_multisample_samples(4);
-
         let canvas = video.window("Veil", width_px, height_px)
-            .opengl()
             .position_centered()
             .build()
             .expect("Failed to create window")
