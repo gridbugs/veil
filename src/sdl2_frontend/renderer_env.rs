2f7c938a10578148a9b9f130c3dc419eabebc538
Stephen Sherratt <stephen@sherra.tt>
2017-06-12 15:47:15 +1000

    Experimental tweaks to sdl settings

--- src/sdl2_frontend/renderer_env.rs
+++ src/sdl2_frontend/renderer_env.rs
@@ -1,6 +1,7 @@
 use sdl2::VideoSubsystem;
 use sdl2::video::WindowContext;
 use sdl2::render::{WindowCanvas, TextureCreator};
+use sdl2::video::GLProfile;
 
 pub struct RendererEnv {
     pub canvas: WindowCanvas,
@@ -9,7 +10,17 @@ pub struct RendererEnv {
 
 impl RendererEnv {
     pub fn new(width_px: u32, height_px: u32, video: &VideoSubsystem) -> Self {
+
+        let gl_attr = video.gl_attr();
+
+        gl_attr.set_context_profile(GLProfile::Core);
+        gl_attr.set_context_flags().debug().set();
+        gl_attr.set_context_version(3, 2);
+        gl_attr.set_multisample_buffers(1);
+        gl_attr.set_multisample_samples(4);
+
         let canvas = video.window("Veil", width_px, height_px)
+            .opengl()
             .position_centered()
             .build()
             .expect("Failed to create window")
